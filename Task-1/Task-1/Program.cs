1947c9c2885cd7b9fafc259c6f8b3f8174bea205
Kirill <kirillmoloch@gmail.com>
2021-04-16 18:54:44 +0300

    Add method for task

--- Task-1/Task-1/Program.cs
+++ Task-1/Task-1/Program.cs
@@ -6,7 +6,54 @@ namespace Task_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Console.WriteLine(CountOfNonRepeatingSymbolsInARow(args[0]));
+            }
+            catch (ArgumentException ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public static int CountOfNonRepeatingSymbolsInARow(string line)
+        {
+            if (line is null)
+            {
+                throw new ArgumentException(nameof(line));
+            }
+
+            int maxCount = 0;
+            int count = 0;
+            char previousSymbol;
+
+            if (line != string.Empty)
+            {
+                previousSymbol = line[0];
+                foreach (char currentSymbol in line)
+                {
+                    if (currentSymbol != previousSymbol)
+                    {
+                        count++;
+                        previousSymbol = currentSymbol;
+                    }
+                    else
+                    {
+                        count = 1;
+                        previousSymbol = currentSymbol;
+                    }
+
+                    if (count > maxCount)
+                    {
+                        maxCount = count;
+                    }
+                }
+
+                return maxCount;
+            }
+
+            return 0;
         }
     }
 }
