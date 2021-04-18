5f639f28d2963bcd8ed8f0b1ff9b1ed7306ad28b
Kirill <kirillmoloch@gmail.com>
2021-04-18 21:09:46 +0300

    Add method for unique symbol task

--- Task-1/Task-1/Program.cs
+++ Task-1/Task-1/Program.cs
@@ -10,18 +10,20 @@ namespace Task_1
             {
                 Console.WriteLine(CountOfNonRepeatingSymbolsInARow(args[0]));
             }
-            catch (ArgumentException ex)
+            catch (ArgumentNullException ex)
             {
 
-                Console.WriteLine(ex.Message);
+                Console.WriteLine("ArgumentNullException" + ex.Message);
             }
+
+            Console.WriteLine(CountOfUniqueSymbolsInARow(args[0]));
         }
 
         public static int CountOfNonRepeatingSymbolsInARow(string line)
         {
             if (line is null)
             {
-                throw new ArgumentException(nameof(line));
+                throw new ArgumentNullException(nameof(line));
             }
 
             int maxCount = 0;
@@ -52,8 +54,65 @@ namespace Task_1
 
                 return maxCount;
             }
+            else
+            {
+                return 0;
+            }
+        }
 
-            return 0;
+        public static int CountOfUniqueSymbolsInARow(string line)
+        {
+            if (line is null)
+            {
+                throw new ArgumentNullException(nameof(line));
+            }
+
+            if (line.Length == 1)
+            {
+                return 1;
+            }
+
+            int maxCount = 0;
+            int count = 0;
+            string lineCopy = string.Empty;
+
+            if (line != string.Empty)
+            {
+                for (int i = 1; i < line.Length; i++)
+                {
+                    lineCopy = lineCopy.Insert(lineCopy.Length, line[i - 1].ToString());
+                    count++;
+
+                    for (int j = 0; j < lineCopy.Length; j++)
+                    {
+                        if (line[i] == lineCopy[j])
+                        {
+                            if (count > maxCount)
+                            {
+                                maxCount = count;
+                            }
+
+                            line = line.Substring(line.IndexOf(line[i]) + 1);
+                            lineCopy = string.Empty;
+                            i = 0;
+                            count = 0;
+
+                            break;
+                        }
+                    }
+
+                    if (count > maxCount)
+                    {
+                        maxCount = count;
+                    }
+                }
+
+                return maxCount;
+            }
+            else
+            {
+                return 0;
+            }
         }
     }
 }
