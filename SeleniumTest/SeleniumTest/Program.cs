353609897005e1196067fd60af1a4de3edcf0e31
Kirill <kirillmoloch@gmail.com>
2021-09-24 12:11:18 +0300

    Selenium

---
+++ SeleniumTest/SeleniumTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace SeleniumTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new FirefoxDriver();
+
+            driver.Navigate().GoToUrl("https://www.mail.ru/");
+
+            IWebElement element = driver.FindElement(By.Name("login"));
+            element.SendKeys("Nigle");
+        }
+    }
+}
